9bd8d28b13c3a0aac19539c4f771c612c8c4c7b4
Takeshi Akimitsu <S17C1003eb@s.chibakoudai.jp>
2022-01-14 05:51:22 +0900

    Add a file

--- mymotor.c
+++ mymotor.c
@@ -5,7 +5,7 @@
 #include<linux/uaccess.h>
 #include<linux/io.h> 
 
-MODULE_AUTHOR("Takeshi Akimitsu");
+MODULE_AUTHOR("Takeshi Akimitsu & Ryuichi Ueda");
 MODULE_DESCRIPTION("driver for motor control"); 
 MODULE_LICENSE("GPL"); 
 MODULE_VERSION("0.0.1");
@@ -16,7 +16,7 @@ static struct class *cls = NULL;
 static volatile u32 *gpio_base = NULL;
 int i;
 
-static ssize_t led_write(struct file* filp, const char* buf, size_t count, loff_t* pos)
+static ssize_t motor_write(struct file* filp, const char* buf, size_t count, loff_t* pos)
 {
 		char c;
 		if(copy_from_user(&c,buf,sizeof(char)))
@@ -31,13 +31,14 @@ static ssize_t led_write(struct file* filp, const char* buf, size_t count, loff_
 		}
 
 		else if(c == 'b'){
-			gpio_base[7] = 1 << 25;
+			gpio_base[10] = 1 << 25;
 			gpio_base[7] = 1 << 27;
 			gpio_base[7] = 1 << 23; 
-			gpio_base[7] = 1 << 24; 
+			gpio_base[10] = 1 << 24; 
 
 		}
 
+ 		else if(c == 's'){                                                                                                               gpio_base[7] = 1 << 25;                                                                                                  gpio_base[7] = 1 << 27;                                                                                                  gpio_base[7] = 1 << 23;                                                                                                  gpio_base[7] = 1 << 24;                                                                                                                                                                                                                   }              
 
 		else if(c == 'l'){ 
 			gpio_base[10] = 1 << 25;
@@ -56,9 +57,9 @@ static ssize_t led_write(struct file* filp, const char* buf, size_t count, loff_
 		return 1;
 }
 
-static struct file_operations led_fops = {
+static struct file_operations motor_fops = {
 		.owner = THIS_MODULE,
-		.write = led_write
+		.write = motor_write
 };
 
 static int __init init_mod(void)
@@ -68,20 +69,20 @@ static int __init init_mod(void)
 	gpio_base = ioremap_nocache(0x3f200000, 0xA0);
 	
 	for(i = 25; i > 22; i--){
-		const u32 led1 = i;
-		const u32 index1 = led1/10;
-		const u32 shift1 = (led1%10)*3;                                                  
-		const u32 mask = ~(0x7 << shift1);
-		gpio_base[index1] = (gpio_base[index1] & mask) | (0x1 << shift1);
+		const u32 motor = i;
+		const u32 index = motor/10;
+		const u32 shift = (motor%10)*3;                                                  
+		const u32 mask = ~(0x7 << shift);
+		gpio_base[index] = (gpio_base[index] & mask) | (0x1 << shift);
 	}
 
-	const u32 led = 27;
-	const u32 index = led/10;
-	const u32 shift = (led%10)*3; 
-	const u32 mask = ~(0x7 << shift);
-	gpio_base[index] = (gpio_base[index] & mask) | (0x1 << shift);	
+	const u32 motor1 = 27;
+	const u32 index1 = motor1/10;
+	const u32 shift1 = (motor1%10)*3; 
+	const u32 mask1 = ~(0x7 << shift1);
+	gpio_base[index1] = (gpio_base[index1] & mask1) | (0x1 << shift1);	
 
-	retval =  alloc_chrdev_region(&dev, 0, 1, "myled");
+	retval =  alloc_chrdev_region(&dev, 0, 1, "mymotor");
 	if(retval < 0){
 		printk(KERN_ERR "alloc_chrdev_region failed.\n");
 		return retval;
@@ -89,7 +90,7 @@ static int __init init_mod(void)
 
 	printk(KERN_INFO "%s is loaded. major:%d\n",__FILE__,MAJOR(dev));
 
-	cdev_init(&cdv, &led_fops);
+	cdev_init(&cdv, &motor_fops);
 	cdv.owner = THIS_MODULE;
 	retval = cdev_add(&cdv, dev, 1); 
 	if(retval < 0){
@@ -97,12 +98,12 @@ static int __init init_mod(void)
 		return retval;
 	}
 
-	cls = class_create(THIS_MODULE,"myled");
+	cls = class_create(THIS_MODULE,"mymotor");
 	if(IS_ERR(cls)){
 		printk(KERN_ERR "class_create failed.");
 		return PTR_ERR(cls);
 	}
-	device_create(cls, NULL, dev, NULL, "myled%d",MINOR(dev));
+	device_create(cls, NULL, dev, NULL, "mymotor%d",MINOR(dev));
 			
 	return 0;
 }
